a42cb36c34a81a647b0d12e6bb8f90c0fd321b95
siphc <ayfpan@ucla.edu>
2025-01-13 21:42:06 -0800

    build version: 8    build date: 2025-01-13 09:42:06 PM

--- configuration.nix
+++ configuration.nix
@@ -98,6 +98,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     git
     obsidian
+    fastfetch
   ];
 
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
