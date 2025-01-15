91165353da3840c22b3b3705c46f91b7829ded25
siphc <ayfpan@ucla.edu>
2025-01-14 16:20:20 -0800

    build version: 1    build date: 2025-01-14 04:20:20 PM

--- configuration.nix
+++ configuration.nix
@@ -95,12 +95,13 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    vim
     git
     obsidian
     fastfetch
-    pywal
+    pywal # wallpaper & palette picker
     vscodium
+    libgcc
   ];
 
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
