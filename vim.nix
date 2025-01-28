4092375d12b3ad8eadd800627b0e7fa6cfd5d2fa
siphc <ayfpan@ucla.edu>
2025-01-27 23:39:24 -0800

    build version: 37     build date: 2025-01-27 11:39:24 PM

--- vim.nix
+++ vim.nix
@@ -2,10 +2,57 @@
 
 {
   environment.systemPackages = with pkgs; [
-  #  (vim-full.customize {
-  #    vimrcConfig.customRC = ''
-  #    '';
-  #  })
-    vim-full
+    (vim-full.customize {
+      vimrcConfig.customRC = ''
+        " reminder! the existence of ~/.vimrc ensures that compatibility mode is
+        " switched off.
+
+        " indentation
+        set tabstop=4
+        set shiftwidth=4
+        set expandtab
+        set smarttab
+        set autoindent
+        set smartindent
+
+        " backspace
+        " allows backspacing over autoindent, line breaks, start of insert
+        set backspace=indent,eol,start
+
+        " vanity
+        set number
+        set relativenumber
+        set title
+        set showmatch
+
+        " status line
+        set laststatus=2
+        set ruler
+
+        " show in-progress commands
+        set showcmd
+
+        " search
+        set ignorecase
+        set smartcase
+
+        " always keep 3 lines above or below the cursor when scrolling, and when not at
+        " the start/end of the file
+        set scrolloff=3
+
+        " 'enable' allows custom color settings with :highlight commands
+        " 'on', in comparison, uses vim default settings
+        syntax enable
+
+        " self explanatory.
+        set noswapfile
+      '';
+    vimrcConfig.packages.myVimPackage = with pkgs.vimPlugins; {
+      start = [
+        vim-colors-solarized
+      ];
+    };
+  })
+   vim-full
   ];
 }
