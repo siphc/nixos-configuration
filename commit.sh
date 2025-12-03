04d94fb5f0dfd505e84c62e5383c304feaacf160
siphc <ayfpan@ucla.edu>
2025-12-03 04:16:49 -0800

    build version: 31 build date: 2025-12-03

--- commit.sh
+++ commit.sh
@@ -10,6 +10,6 @@ fi
 git diff -bU0
 
 message=$(nixos-rebuild list-generations | grep True)
-bversion=echo $message | awk {'print $1'}
-bdate=echo $message | awk {'print $2'}
+bversion=$(echo $message | awk {'print $1'})
+bdate=$(echo $message | awk {'print $2'})
 git commit -am "build version: $bversion build date: $bdate"
