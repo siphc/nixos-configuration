f7f4d5dd425e6a3b4b9728ac585cd90618f08f14
siphc <ayfpan@ucla.edu>
2025-12-03 04:13:43 -0800

    build version:  build date:

--- commit.sh
+++ commit.sh
@@ -7,5 +7,7 @@ fi
 
 git diff -bU0
 
-message=$(nixos-rebuild list-generations | grep current)
-git commit -am "build version: ${message:0:3}    build date: $(date +%F\ %r)"
+message=$(nixos-rebuild list-generations | grep True)
+bversion=echo $message | awk {'print $1'}
+bdate=echo $message | awk {'print $2'}
+git commit -am "build version: $bversion build date: $bdate"
