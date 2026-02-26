58efbfbbd7264f407591691b26ece36811927e4c
siphc <ayfpan@ucla.edu>
2026-02-25 19:45:30 -0800

    feat: Config directory refactoring

--- initsetup.sh
+++ initsetup.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+rm -rf ~/.config/helix ~/.config/alacritty
+rm ~/.config/lf ~/.bashrc ~/.local/bin
+
 mkdir -p ~/.config/helix ~/.config/alacritty ~/.trash ~/Pictures/Screenshots ~/.local
 ln -s ~/nixos-configuration/.bashrc ~/.bashrc
 ln -s ~/nixos-configuration/bin ~/.local/bin
-ln -s ~/nixos-configuration/configs/lf ~/.config/lf
-ln -s ~/nixos-configuration/configs/hx-config.toml ~/.config/helix/config.toml
-ln -s ~/nixos-configuration/configs/hx-language.toml ~/.config/helix/languages.toml
-ln -s ~/nixos-configuration/configs/alacritty.toml ~/.config/alacritty/alacritty.toml
+ln -s ~/nixos-configuration/cfgs/lf ~/.config/lf
+ln -s ~/nixos-configuration/cfgs/hx-config.toml ~/.config/helix/config.toml
+ln -s ~/nixos-configuration/cfgs/hx-language.toml ~/.config/helix/languages.toml
+ln -s ~/nixos-configuration/cfgs/alacritty.toml ~/.config/alacritty/alacritty.toml
