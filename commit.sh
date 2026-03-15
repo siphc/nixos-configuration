c42328695d1e88ffa10cb561fe26e97d447918d7
siphc <ayfpan@ucla.edu>
2026-03-14 18:37:47 -0700

    darwin: update commit.sh to be polymorphic

--- commit.sh
+++ commit.sh
@@ -9,7 +9,15 @@ fi
 
 git diff -bU0
 
-message=$(nixos-rebuild list-generations | grep True)
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	message=$(nixos-rebuild list-generations | grep True)
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	message=$(sudo darwin-rebuild --list-generations | grep current)
+else
+	echo "Unsupported OS."
+	exit 0
+fi
+
 bversion=$(echo $message | awk {'print $1'})
 bdate=$(echo $message | awk {'print $2'})
 git commit -am "build version: $bversion build date: $bdate"
