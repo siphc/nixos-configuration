2d6fbe0a627d1f46c17df1d410d5133168263655
siphc <ayfpan@ucla.edu>
2026-03-14 18:23:14 -0700

    darwin: init

---
+++ darwin-config.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, ... }:
+
+{
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+    git
+    helix
+    librewolf
+    alacritty
+  ];
+
+  # If you change this, next invocation of darwin-rebuild should use the -I flag.
+  environment.darwinConfig = "/Users/svi/Documents/nixos-configuration/darwin-config.nix";
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 6;
+}
